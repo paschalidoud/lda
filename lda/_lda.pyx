ec97ac9f7d691bee7c1342ffd8d7838a394b8026
Allen Riddell <abr@ariddell.org>
2015-07-12 15:38:52 -0400

    WIP release the GIL during sampling
    Appears to decrease performance (very slightly)

--- lda/_lda.pyx
+++ lda/_lda.pyx
@@ -8,16 +8,17 @@ from libc.stdlib cimport malloc, free
 
 
 cdef extern from "gamma.h":
-    cdef double lda_lgamma(double x)
+    cdef double lda_lgamma(double x) nogil
 
 
-cdef double lgamma(double x):
+cdef double lgamma(double x) nogil:
     if x <= 0:
-        raise ValueError("x must be strictly positive")
+        with gil:
+            raise ValueError("x must be strictly positive")
     return lda_lgamma(x)
 
 
-cdef int searchsorted(double* arr, int length, double value):
+cdef int searchsorted(double* arr, int length, double value) nogil:
     """Bisection search (c.f. numpy.searchsorted)
 
     Find the index into sorted array `arr` of length `length` such that, if
@@ -44,36 +45,38 @@ def _sample_topics(int[:] WS, int[:] DS, int[:] ZS, int[:, :] nzw, int[:, :] ndz
     cdef int n_rand = rands.shape[0]
     cdef int n_topics = nz.shape[0]
     cdef double eta_sum = 0
-    for i in range(eta.shape[0]):
-        eta_sum += eta[i]
     cdef double* dist_sum = <double*> malloc(n_topics * sizeof(double))
     if dist_sum is NULL:
         raise MemoryError("Could not allocate memory during sampling.")
 
-    for i in range(N):
-        w = WS[i]
-        d = DS[i]
-        z = ZS[i]
+    with nogil:
+        for i in range(eta.shape[0]):
+            eta_sum += eta[i]
 
-        dec(nzw[z, w])
-        dec(ndz[d, z])
-        dec(nz[z])
+        for i in range(N):
+            w = WS[i]
+            d = DS[i]
+            z = ZS[i]
 
-        dist_cum = 0
-        for k in range(n_topics):
-            # eta is a double so cdivision yields a double
-            dist_cum += (nzw[k, w] + eta[w]) / (nz[k] + eta_sum) * (ndz[d, k] + alpha[k])
-            dist_sum[k] = dist_cum
+            dec(nzw[z, w])
+            dec(ndz[d, z])
+            dec(nz[z])
 
-        r = rands[i % n_rand] * dist_cum  # dist_cum == dist_sum[-1]
-        z_new = searchsorted(dist_sum, n_topics, r)
+            dist_cum = 0
+            for k in range(n_topics):
+                # eta is a double so cdivision yields a double
+                dist_cum += (nzw[k, w] + eta[w]) / (nz[k] + eta_sum) * (ndz[d, k] + alpha[k])
+                dist_sum[k] = dist_cum
 
-        ZS[i] = z_new
-        inc(nzw[z_new, w])
-        inc(ndz[d, z_new])
-        inc(nz[z_new])
+            r = rands[i % n_rand] * dist_cum  # dist_cum == dist_sum[-1]
+            z_new = searchsorted(dist_sum, n_topics, r)
 
-    free(dist_sum)
+            ZS[i] = z_new
+            inc(nzw[z_new, w])
+            inc(ndz[d, z_new])
+            inc(nz[z_new])
+
+        free(dist_sum)
 
 
 cpdef double _loglikelihood(int[:, :] nzw, int[:, :] ndz, int[:] nz, int[:] nd, double alpha, double eta):
@@ -88,19 +91,20 @@ cpdef double _loglikelihood(int[:, :] nzw, int[:, :] ndz, int[:] nz, int[:] nd, 
     cdef double lgamma_eta = lgamma(eta)
     cdef double lgamma_alpha = lgamma(alpha)
 
-    ll += n_topics * lgamma(eta * vocab_size)
-    for k in range(n_topics):
-        ll -= lgamma(eta * vocab_size + nz[k])
-        for w in range(vocab_size):
-            # if nzw[k, w] == 0 addition and subtraction cancel out
-            if nzw[k, w] > 0:
-                ll += lgamma(eta + nzw[k, w]) - lgamma_eta
-
-    # calculate log p(z)
-    for d in range(D):
-        ll += (lgamma(alpha * n_topics) -
-                lgamma(alpha * n_topics + nd[d]))
+    with nogil:
+        ll += n_topics * lgamma(eta * vocab_size)
         for k in range(n_topics):
-            if ndz[d, k] > 0:
-                ll += lgamma(alpha + ndz[d, k]) - lgamma_alpha
-    return ll
+            ll -= lgamma(eta * vocab_size + nz[k])
+            for w in range(vocab_size):
+                # if nzw[k, w] == 0 addition and subtraction cancel out
+                if nzw[k, w] > 0:
+                    ll += lgamma(eta + nzw[k, w]) - lgamma_eta
+
+        # calculate log p(z)
+        for d in range(D):
+            ll += (lgamma(alpha * n_topics) -
+                    lgamma(alpha * n_topics + nd[d]))
+            for k in range(n_topics):
+                if ndz[d, k] > 0:
+                    ll += lgamma(alpha + ndz[d, k]) - lgamma_alpha
+        return ll
